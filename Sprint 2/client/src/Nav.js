81af3133d1ce8afa4f8fcc61ab45f81224b5b5d8
Shasheen_B <50681308+Shasheen8@users.noreply.github.com>
2020-03-13 19:33:58 -0400

    Update Nav.js

--- Sprint 2/client/src/Nav.js
+++ Sprint 2/client/src/Nav.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
 
+//Navigation bar on the home page. 
+
 function Nav () {
     const navStyle = {
         color: 'White'
