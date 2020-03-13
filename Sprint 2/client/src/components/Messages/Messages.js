63bc8d8eb6f3ded25842fa92bf6160bf262782aa
Shasheen_B <50681308+Shasheen8@users.noreply.github.com>
2020-03-13 19:30:45 -0400

    Update Messages.js

--- Sprint 2/client/src/components/Messages/Messages.js
+++ Sprint 2/client/src/components/Messages/Messages.js
@@ -6,6 +6,8 @@ import Message from '../Message/Message';
 
 import './Messages.css';
 
+//Each individual message sent by the user that has signed up through the join component 
+
 const Messages = ({ messages, name }) => (
   <ScrollToBottom className= "messages">
     {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
