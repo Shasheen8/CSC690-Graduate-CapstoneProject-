7d2dc42b058a61bdab166c871fe635a2f175aae0
Shasheen_B <50681308+Shasheen8@users.noreply.github.com>
2020-05-05 17:18:17 -0400

    Update FBAuth.js

--- Sprint 5 final/Client/src/FBAuth.js
+++ Sprint 5 final/Client/src/FBAuth.js
@@ -3,7 +3,8 @@ import React from 'react';
 const firebase = require("firebase");
 
 //MiddleWare for routes using tokens
-// Using token authorization for middle ware authentication. 
+// Using token authorization for middle ware authentication.
+
 const FBAuth = (req, res, next) => {
     let idToken;
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
