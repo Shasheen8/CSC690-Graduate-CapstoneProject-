39def315598332d39c2b301d7bfb17fb260fe636
Shasheen_B <50681308+Shasheen8@users.noreply.github.com>
2020-05-05 18:46:52 -0400

    Update Input.js

--- Sprint 5 final/Client/src/components/Input/Input.js
+++ Sprint 5 final/Client/src/components/Input/Input.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import './Input.css';
 
+
+// For a user to send a message once typed in the message bar.
+
 const Input = ({ message, setMessage, sendMessage }) => (
   <form className="form">
     <input
