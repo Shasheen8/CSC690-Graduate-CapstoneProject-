86077a42cf5a53396aa375cccc272004c7aeb9ca
Shasheen_B <50681308+Shasheen8@users.noreply.github.com>
2020-05-05 18:49:10 -0400

    Update Message.js

--- Sprint 5 final/Client/src/components/Message/Message.js
+++ Sprint 5 final/Client/src/components/Message/Message.js
@@ -3,6 +3,10 @@ import React from 'react';
 import './Message.css';
 import ReactEmoji from 'react-emoji';
 
+
+// Message by curent user in the chat room
+
+
 const Message = ({ message: { user, text }, name }) => {
   let isSentByCurrentUser = false;
 
