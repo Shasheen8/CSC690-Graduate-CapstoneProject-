307ff1b38643d3490b6950fc27f0310e298fa558
Shasheen_B <50681308+Shasheen8@users.noreply.github.com>
2020-05-10 23:23:46 -0400

    Update login.js

--- Sprint 5 final/Client/src/login/login.js
+++ Sprint 5 final/Client/src/login/login.js
@@ -9,7 +9,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-
+import fbAuth from './fbAuth.js';
 const firebase = require("firebase");
 
 
